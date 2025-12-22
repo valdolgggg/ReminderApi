6d0e2721761d80f8f64f1ae32424ae02e592b2de
valeria <valeria@WIN-VMF3HVMEK23>
2025-12-22 21:38:18 +0300

    Добавьте файлы проекта.

---
+++ ReminderApi/Controllers/ReminderController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReminderApi.Models;
+using System.Collections.Generic;
+
+namespace ReminderApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReminderController : ControllerBase
+    {
+        // Временное хранилище напоминаний
+        private static List<Reminder> reminders = new List<Reminder>();
+
+        // Получение списка всех напоминаний
+        [HttpGet]
+        public ActionResult<List<Reminder>> GetAllReminders()
+        {
+            return Ok(reminders);
+        }
+
+        // Создание нового напоминания
+        [HttpPost]
+        public ActionResult<Reminder> CreateReminder(Reminder reminder)
+        {
+            reminders.Add(reminder);
+            return Ok(reminder);
+        }
+    }
+}
---
+++ ReminderApi/Models/Reminder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ReminderApi.Models
+{
+    public class Reminder
+    {
+        // Уникальный идентификатор напоминания
+        public int Id { get; set; }
+
+        // Название напоминания
+        public string Title { get; set; }
+
+        // Описание напоминания
+        public string Description { get; set; }
+
+        // Дата и время, когда должно сработать напоминание
+        public DateTime DateTime { get; set; }
+
+        // Приоритет напоминания (например: низкий, средний, высокий)
+        public string Priority { get; set; }
+
+        // Статус напоминания (создано, выполнено и т.д.)
+        public string Status { get; set; }
+    }
+}
