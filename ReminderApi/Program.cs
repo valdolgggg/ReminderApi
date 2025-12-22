efebee95ceb14002ec5dcc01565d4dfb48b586db
Valeriia Dolgosheeva <vdolgosheeva@sfedu.ru>
2025-12-23 02:06:01 +0300

    Fix startup

--- ReminderApi/Program.cs
+++ ReminderApi/Program.cs
@@ -1,25 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// ◊“≈Õ»≈ œŒ–“¿ »« Œ –”∆≈Õ»ﬂ (Í‡Í ÚÂ·Û˛Ú Ó·Î‡Í‡)
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
+
+// Fix startup for Amvera
+
+// Commit test for Amvera deployment
