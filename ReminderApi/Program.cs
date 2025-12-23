0a9330f41a9d9b8cf058dfcd55d98098ce219c6a
Valeriia Dolgosheeva <vdolgosheeva@sfedu.ru>
2025-12-23 15:00:01 +0300

    Fix startup for Amvera container

--- ReminderApi/Program.cs
+++ ReminderApi/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// ×ÒÅÍÈÅ ÏÎĞÒÀ ÈÇ ÎÊĞÓÆÅÍÈß (êàê òğåáóşò îáëàêà)
+// ÂÀÆÍÎ äëÿ Amvera
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
@@ -10,15 +10,9 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers();
+
 app.Run();
-
-// Fix startup for Amvera
-
-// Commit test for Amvera deployment
